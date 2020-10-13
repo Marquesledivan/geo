45cb7fb91e9a54a6e368960f81679595737243f6
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2020-10-12 22:06:58 -0300

    Create clean_ecr_v1.py

---
+++ clean_ecr_v1.py
@@ -0,0 +1,21 @@
+#!/bin/python2.7
+# -*- coding: utf-8 -*-
+"""
+version 1.0 Author: Ledivan B. Marques
+            Email:	ledivan@cvc.com.br
+
+"""
+
+import subprocess
+
+result = subprocess.check_output("""aws ecr describe-repositories --query 'repositories[*].repositoryName' --profile desenvolvimento | jq .[] | sed 's|"||g'""", shell=True)
+
+def ecr():
+     for repo in result.splitlines():         
+         TAGS=subprocess.check_output("""aws ecr list-images --region sa-east-1 --repository-name """ + repo + """ --filter tagStatus=UNTAGGED --query 'imageIds[].imageDigest' --profile desenvolvimento | jq .[] | sed 's|"||g'""",shell=True)
+         for tag in TAGS.splitlines():
+             print("Deleting image:" + tag)
+             subprocess.call("aws ecr batch-delete-image --region sa-east-1 --repository-name " + repo + " --image-ids imageDigest="+tag+" --profile desenvolvimento",shell=True)
+        
+if __name__ == "__main__":
+     ecr()
