ccf5030809e2e16af6e9e94e3b2bf0e1db73f58a
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2020-10-12 22:04:27 -0300

    Create delete_untagged_images_ecr.py

---
+++ delete_untagged_images_ecr.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+version 1.0 Author: Ledivan B. Marques
+            Email:	ledivan@cvc.com.br
+
+"""
+import boto3
+
+session = boto3.Session(profile_name='ti-sa')
+client = session.client('ecr')
+
+def Untagged():
+  describe = client.describe_repositories()
+  try:
+    for i in describe['repositories']:
+      print("repository:", i['repositoryUri'])
+      response = client.list_images(registryId=i['registryId'],repositoryName=i['repositoryName'],filter={'tagStatus': 'UNTAGGED'})
+      for imageDigest in response["imageIds"]:
+        print("imageDigest", imageDigest['imageDigest'])
+        print("Delete images Untagged from ECR",imageDigest['imageDigest'])
+        client.batch_delete_image(registryId=i['registryId'],repositoryName=i['repositoryName'],imageIds=[{'imageDigest':imageDigest['imageDigest']},])
+  except Exception as a:
+    print("Erro {0}".format(a))
+
+if __name__ == "__main__":
+  Untagged()
