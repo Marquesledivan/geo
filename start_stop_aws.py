3510d46136fa2c2b1e908ca7640468096921002a
Ledivan Bernardo Marques <ledivan_bernardo@yahoo.com.br>
2021-01-27 22:51:54 -0300

    aws_start.py

--- start_stop_aws.py
+++ start_stop_aws.py
@@ -8,11 +8,16 @@ import boto3
 import pprint
 import json
 import sys, traceback
+import argparse
 
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
 
 instanceIds = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--type', help='Check the get, start or stop parameters')
+args = parser.parse_args()
 
 def get_instances():
   for r in response["Reservations"]:
@@ -25,27 +30,40 @@ def get_instances():
                   print (f'InstanceType: {t} name: {v}  state: {s} ')
 
 def get_start():
+    response = ec2client.describe_instances()
     for reservation in response['Reservations']:
           for response in reservation['Instances']:
-                      for state in response["State"]["Name"]:
-                            try:
-                              if response['State']['Name'] == "stopped":
-                                    instanceIds.append(response['InstanceId'])
-                            except:
-                                  print ("Not expected error: ", traceback.print_exc())
+                for state in response["State"]["Name"]:
+                      try:
+                            if response['State']['Name'] == "stopped":
+                                  instanceIds.append(response['InstanceId'])
+                      except:
+                            print ("Not expected error: ", traceback.print_exc())
           if len(instanceIds) > 0:
               print ("Starting instances: " + str(instanceIds))
-              ec2client.stop_instances(InstanceIds=instanceIds)
+              ec2client.start_instances(InstanceIds=instanceIds)
 
 def get_stop():
+    response = ec2client.describe_instances()
     for reservation in response['Reservations']:
           for response in reservation['Instances']:
-                      for state in response["State"]["Name"]:
-                            try:
-                              if response['State']['Name'] == "running":
-                                    instanceIds.append(response['InstanceId'])
-                            except:
-                                  print ("Not expected error: ", traceback.print_exc())
+                for state in response["State"]["Name"]:
+                      try:
+                            if response['State']['Name'] == "running":
+                                  instanceIds.append(response['InstanceId'])
+                      except:
+                            print ("Not expected error: ", traceback.print_exc())
           if len(instanceIds) > 0:
               print ("Stopping instances: " + str(instanceIds))
               ec2client.stop_instances(InstanceIds=instanceIds)
+
+if __name__ == "__main__":
+    print('Starting instances... ')
+    if args.type == "get":
+          get_instances()
+    elif args.type == "start":
+          get_start()
+    elif args.type ==  "stop":
+          get_stop()
+    else:
+      print("Please check the parameters --type: get, start or stop")
