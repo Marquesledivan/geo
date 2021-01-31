07c73ad17d8a6f5c8152ed78aeb74522cb58fd7f
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-01-30 23:19:09 -0300

    Update puppet_ tasks_api.py

--- puppet_ tasks_api.py
+++ puppet_ tasks_api.py
@@ -13,6 +13,8 @@ import os
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
+jobs = []
+
 def login_api():
      user = input("Digite seu user:")
      passwd = getpass.getpass("Digite sua senha: ")
@@ -45,7 +47,21 @@ def run_task(command,nodes):
      data = {"task":"bolt_shim::command","params":{"command": command },"scope":{"nodes":[nodes]}}
      payload = json.dumps(data)
      r = requests.post("https://master.ledivan.com.br:8143/orchestrator/v1/command/task",headers=headers,data=payload,verify=False)
-     print(json.loads(r.text)["job"]["id"])
+     jobs.append(json.loads(r.text)["job"]["name"])
+
+
+def orchestrator_jobs(jobs):
+     token = check_token()
+     headers={'Content-type': 'application/json', 'X-Authentication': token}
+     r = requests.get(f'https://master.ledivan.com.br:8143/orchestrator/v1/jobs/{jobs}?limit=20&offset=20',headers=headers,verify=False)
+     i = json.loads(r.text)
+     print(i["options"]["scope"]["nodes"],i["state"])
+
 
 if __name__ == "__main__":
-    run_task("ls -lha", "master.ledivan.com.br")
+    f = open("hosts.txt", "r")
+    hosts = f.read()
+    for i in hosts.splitlines():
+        run_task("ls -lha", i)
+    for i in jobs:
+        orchestrator_jobs(i)
