0a8557ac7a941ddc2c3ff51a8b57b42b45b2e01e
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-02-02 20:42:33 -0300

    Update decode.py

--- decode.py
+++ decode.py
@@ -11,7 +11,7 @@ import base64
 r = requests.get("http://127.0.0.1:8500/v1/kv/ledivan/ledivan/chave")
 d = str(json.loads(r.text)[0]["Value"])
 print(base64.b64decode(d).decode("ascii"))
-
+### TypeError: list indices must be integers or slices, not str
 ########
 
 encoded = 'YmFzZTY0IGVuY29kZWQgc3RyaW5n'
