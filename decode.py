61708fa7e5f1d0621bc742102edaa74ddc659eef
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-02-02 20:41:28 -0300

    Update decode.py

--- decode.py
+++ decode.py
@@ -11,3 +11,9 @@ import base64
 r = requests.get("http://127.0.0.1:8500/v1/kv/ledivan/ledivan/chave")
 d = str(json.loads(r.text)[0]["Value"])
 print(base64.b64decode(d).decode("ascii"))
+
+########
+
+encoded = 'YmFzZTY0IGVuY29kZWQgc3RyaW5n'
+data = base64.b64decode(encoded)
+print(data.decode("ascii"))
