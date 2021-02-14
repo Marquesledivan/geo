347f05f395070f864fa2e2441f45e1ba4bd8e8f4
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-02-14 19:09:43 -0300

    Update flask-api_aws.py

--- flask-api_aws.py
+++ flask-api_aws.py
@@ -15,43 +15,55 @@ client = boto3.client('ec2')
 reponse = client.describe_instances()
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-lista = []
 @app.route('/', methods=['GET'])
 def get_nome():
-    lista.clear()
+    lista = []
     if request.method == 'GET':
         for i in reponse["Reservations"]:
             for instace in i["Instances"]:
-                ids = instace["InstanceId"] + ', ' + instace["State"]["Name"]
-                lista.append(ids)
+                for tag in instace["Tags"]:
+                    if tag["Key"] == 'Name':
+                        ids = instace["InstanceId"] + ', ' + instace["State"]["Name"] + ', ' + tag['Value']
+                        lista.append(ids)
         return jsonify(lista)
 
-stopped = "stopped"
 @app.route("/stopped", methods=['POST'])
 def stop():
+    stopped = "stopped"
     if request.method == 'POST':
         if request.is_json:
             content = request.get_json()
+            tags = content["InstanceIds"]["tags"]
             instancia = content["InstanceIds"]["id"]
             try:
                 for i in instancia:
                     state = client.describe_instances(InstanceIds=[i])
-                    if state["ResponseMetadata"]["HTTPStatusCode"] == 200:
-                        if content['InstanceIds']['state'] == stopped:
-                            try:
-                                if len(instancia) > 0:
-                                    for i in instancia:
-                                        client.stop_instances(InstanceIds=[i],DryRun=False)
-                            except ClientError as e:
-                                if 'DryRunOperation' not in str(e):
-                                    raise
-                            finally:
-                                return jsonify(stopped,instancia), status.HTTP_201_CREATED
+                    for i in state["Reservations"]:
+                        for st in i["Instances"]:
+                            for tag in st["Tags"]:
+                                if tag['Key'] == 'Plataforma' and tag['Value'] == tags:
+                                    if state["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                                        if content['InstanceIds']['state'] == stopped:
+                                            try:
+                                                if len(instancia) > 0:
+                                                    for i in instancia:
+                                                        print(i)
+                                                        client.stop_instances(InstanceIds=[i],DryRun=False)
+                                            except ClientError as e:
+                                                if 'DryRunOperation' not in str(e):
+                                                        raise
+                                            finally:
+                                               return jsonify(stopped,instancia), status.HTTP_201_CREATED
+                                        else:
+                                            return "HTTP_404_NOT_FOUND", status.HTTP_404_NOT_FOUND
+                                else:
+                                    return "TAG_IS_NOT_SUPPORTED_NOT_FOUND", status.HTTP_404_NOT_FOUND
             except botocore.exceptions.ClientError as e:
-                return "HTTP_404_NOT_FOUND", status.HTTP_404_NOT_FOUND
+                return "INSTANCIA_NOT_FOUND", status.HTTP_404_NOT_FOUND
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-#### curl --location --request POST 'http://127.0.0.1:5000/stopped' --header 'Content-Type: application/json' --data-raw '{ "InstanceIds": { "state": "stopped", "id": ["i-00000000","i-111111111"] }}'
+#### curl --location --request POST 'http://127.0.0.1:5000/stopped' --header 'Content-Type: application/json' --data-raw '{ "InstanceIds": { "state": "stopped", "id": ["i-00000000","i-111111111"],"tags": "Infra" }}'
