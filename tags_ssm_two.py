a2db7ed6077d79b4365515966f969c2dc54e789c
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-03-07 18:14:58 -0300

    Update tags_ssm_two.py

--- tags_ssm_two.py
+++ tags_ssm_two.py
@@ -6,43 +6,50 @@ version 1.0 Author: Ledivan B. Marques
 import boto3
 import botocore
 import pprint
-client = boto3.client("ssm")
+REGION = ['us-east-1',"us-east-1"]
 
-response = client.get_paginator('describe_parameters')
-paginator = response.paginate().build_full_result()
 
 lista = []
-dirs = {'Key': "string", "Value": "string"}
-for page in paginator['Parameters']:
-    response = client.get_parameter(Name=page['Name'])
-    value = response['Parameter']['Value']
-    lista.append(page['Name'])
+tgs_name = {'Key': "string", "Value": "string"}
 
-TGS = {}
-def get_tags(i):
+def region_get(regions):
+    client = boto3.client("ssm",region_name=regions)
+    response = client.get_paginator('describe_parameters')
+    paginator = response.paginate().build_full_result()
+
+    for page in paginator['Parameters']:
+        response = client.get_parameter(Name=page['Name'])
+        value = response['Parameter']['Value']
+        lista.append(page['Name'])
+
+def get_tags(name,regions):
     string = "string"
+    client = boto3.client("ssm",region_name=regions)
     tags = client.list_tags_for_resource(
         ResourceType='Parameter',
-        ResourceId=i
+        ResourceId=name
     )
     for r in tags['TagList']:
         if "string" in r["Key"]:
             tag = {'Key': string, "Value": r["Value"]}
             return tag
-    return dirs
+    return tgs_name
 
-def set_tags(TGS):
+def set_tags(tgs,name,regions):
+    client = boto3.client("ssm",region_name=regions)
     tags = client.list_tags_for_resource(
         ResourceType='Parameter',
-        ResourceId=i
+        ResourceId=name
     )
     try:
-        if TGS not in tags['TagList']:
-            print(i)
-            client.add_tags_to_resource(ResourceType='Parameter',ResourceId=i,Tags=[TGS])
+        if tgs not in tags['TagList']:
+            client.add_tags_to_resource(ResourceType='Parameter',ResourceId=name,Tags=[tgs])
+            print(name,tgs)
     except botocore.exceptions.ParamValidationError:
-        print(i)
-        client.add_tags_to_resource(ResourceType='Parameter',ResourceId=i,Tags=[TGS])
+        client.add_tags_to_resource(ResourceType='Parameter',ResourceId=name,Tags=[tgs])
+        print(name,tgs)
 
-for i in lista:
-    set_tags(get_tags(i))
+for reg in REGION:
+    region_get(reg)
+    for parameter in lista:
+        set_tags(get_tags(parameter,reg),parameter,reg)
