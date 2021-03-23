fd6fd3c98b8f1721f86b2bcc9badd2c7072559cd
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-03-23 17:09:39 -0300

    Update ansible.py

--- ansible.py
+++ ansible.py
@@ -1,7 +1,10 @@
-ansible linux -i inventory -m ansible.builtin.shell -a 'systemctl start httpd' -u ledivan --ask-pass -b -K
+# ansible.builtin.shell
+ansible linux -i inventory -m shell -a 'systemctl start httpd' -u ledivan --ask-pass -b -K
 
-ansible linux -i inventory -m ansible.builtin.service -a "name=httpd state=restarted" -u ledivan --ask-pass -b -K
+# ansible.builtin.service
+ansible linux -i inventory -m service -a "name=httpd state=restarted" -u ledivan --ask-pass -b -K
 
+# ansible.builtin.copy
 ansible linux -i inventory -m copy -a 'src=/Users/ledivan/temp/inventory dest=/root owner=root mode=0644' -u ledivan --ask-pass -b -K
 
 cat inventory
