b4b5d4945ff0137da0be1b17c5e0f1be1aaa2c60
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-03-24 00:08:39 -0300

    Update ansible.py

--- ansible.py
+++ ansible.py
@@ -8,7 +8,27 @@ ansible linux -i inventory -m service -a "name=httpd state=restarted" -u ledivan
 ansible linux -i inventory -m copy -a 'src=/Users/ledivan/temp/inventory dest=/root owner=root mode=0644' -u ledivan --ask-pass -b -K
 
 cat inventory
-
 [linux]
 192.168.1.53
 192.168.1.20
+
+~/.ansible.cfg
+[defaults]
+ansible_python_interpreter=/usr/bin/python3
+inventory = /Users/ledivan/temp/inventory
+vault_password_file = ~/.ansible/vault/password.txt
+remote_user = ledivan
+##ask_sudo_pass = True
+##ask_pass      = True
+deprecation_warnings = False
+command_warnings = False
+host_key_checking = False
+remote_port = 22
+
+[privilege_escalation]
+become=True
+become_method=sudo
+become_ask_pass=False
+
+# ansible.builtin.service
+ansible linux -m service -a "name=httpd state=restarted"  -K
