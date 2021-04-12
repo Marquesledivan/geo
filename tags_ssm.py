55e22fc3018714dd71d53e39f22faebfb2c68bf1
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-04-12 16:50:15 -0300

    Update tags_ssm.py

--- tags_ssm.py
+++ tags_ssm.py
@@ -6,42 +6,47 @@ version 1.0 Author: Ledivan B. Marques
 import boto3
 import botocore
 import pprint
-client = boto3.client("ssm")
+ec2 = boto3.client('ec2')
+regions = ec2.describe_regions()
 
-response = client.get_paginator('describe_parameters')
-paginator = response.paginate().build_full_result()
-
-lista = []
 tgs_name = {'Key': "string", "Value": "string"}
-for page in paginator['Parameters']:
-    response = client.get_parameter(Name=page['Name'])
-    value = response['Parameter']['Value']
-    lista.append(page['Name'])
 
-def get_tags(name):
+def parameter_by_region(client):
+    lista = []
+    client = boto3.client("ssm",region_name=region['RegionName'])
+    response = client.get_paginator('describe_parameters')
+    paginator = response.paginate().build_full_result()
+    for page in paginator['Parameters']:
+        response = client.get_parameter(Name=page['Name'])
+        value = response['Parameter']['Value']
+        lista.append(page['Name'])
+    return lista
+
+def get_tags(name,client):
     string = "string"
     tags = client.list_tags_for_resource(
         ResourceType='Parameter',
         ResourceId=name
     )
     for r in tags['TagList']:
-        if "string" in r["Key"]:
-            tag = {'Key': string, "Value": r["Value"]}
-            return tag
-    return tgs_name
+        if len(tags['TagList']) > 0 and "string" in r["Key"]:
+            return True
+    return False
 
-def set_tags(tgs,name):
+
+def set_tags(get_tags,name,client):
     tags = client.list_tags_for_resource(
         ResourceType='Parameter',
         ResourceId=name
     )
-    try:
-        if tgs not in tags['TagList']:
-            print(name,tgs)
-            client.add_tags_to_resource(ResourceType='Parameter',ResourceId=name,Tags=[tgs])
-    except botocore.exceptions.ParamValidationError:
-        print(name,tgs)
-        client.add_tags_to_resource(ResourceType='Parameter',ResourceId=name,Tags=[tgs])
+    if not get_tags:
+        print(f"Building {name} tags............")
+        client.add_tags_to_resource(ResourceType='Parameter',ResourceId=name,Tags=[tgs_name])
 
-for parameter in lista:
-    set_tags(get_tags(parameter),parameter)
+if __name__ == "__main__":
+    for region in regions['Regions']:
+        print("Region: " + str(region['RegionName']))
+        client = boto3.client("ssm",region_name=region['RegionName'])
+        for parameter_name in parameter_by_region(client):
+            tags_owner = get_tags(parameter_name,client)
+            set_tags(tags_owner,parameter_name,client)
