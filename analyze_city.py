7c196c7d678609fb3ce394132eb4e12f6b533bfb
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2021-06-09 22:11:39 -0300

    Update analyze_city.py

--- analyze_city.py
+++ analyze_city.py
@@ -6,7 +6,7 @@ version 1.0 Author: Ledivan B. Marques
 """
 import csv
 import requests
-from urllib import request
+import urllib.request
 import os
 import smtplib
 from email.mime.text import MIMEText
@@ -14,59 +14,51 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 import sys
 
-city =  sys.argv[1]
+arg =  sys.argv[1]
 
 url_ibge = r"http://blog.mds.gov.br/redesuas/wp-content/uploads/2018/06/Lista_Munic%C3%ADpios_com_IBGE_Brasil_Versao_CSV.csv"
-html = f"""
-<html>
-<body>
-<img src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Rua_do_Conjunto_Arquitet%C3%B4nico_da_Cidade_de_Goi%C3%A1s%2C_Goi%C3%A1s%2C_Brasil.jpg" width="522" height="346">
-</body>
-</html>
-<html> <body> <p>Hi,<br>Check out the new post on the api city blog:</p> <p><a href="https://servicodados.ibge.gov.br/api/v1/localidades/estados/{city.upper()}">Test API: Cloud-based or City API!!</a></p> <p> Feel free to <strong>let us</strong> know what content would be useful for you!</p> </body> </html> <pre>
-"""
 
 def states(sigla):
       response = requests.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
       for siglas in response.json():
             if sigla in siglas["sigla"]:
                   return siglas["nome"]
-def get(url):
-      count = 0
-      list_cidades = []
-      sigla_city = states(city.upper())
-      if os.path.exists("demofile.txt"):
-            os.remove("demofile.txt")
-      f = open("demofile.txt", "a")
-      f.write(html)
-      f.write("""<p style="font-size:30px">"""+sigla_city +"""</p>""")
-      with request.urlopen(url) as entrada:
+def get(sigla):
+      lista = ""
+      with urllib.request.urlopen(url_ibge) as entrada:
         print("Downloading data for analysis")
         dados = entrada.read().decode('latin-1')
         for cidade in csv.reader(dados.splitlines(True)):
-              sigla = f'{cidade[0]}'
-              ci_sigla = sigla.split(";")
-              if city.upper() in  ci_sigla:
-                    f = open("demofile.txt", "a")
-                    f.write('\n'+ci_sigla[0][2:])
-                    f.close()
+              si = f'{cidade[0]}'
+              ci_sigla = si.split(";")
+              if sigla.upper() in  ci_sigla:
+                    lista += ('\n'+ci_sigla[0][2:])
+        return lista
 
-def send_email(url):
+def send_email(sigla_city):
+      sigla = states(sigla_city.upper())
+      body = str(get(sigla_city.upper()))
+      html = f"""
+            <html>
+            <body>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Rua_do_Conjunto_Arquitet%C3%B4nico_da_Cidade_de_Goi%C3%A1s%2C_Goi%C3%A1s%2C_Brasil.jpg" width="522" height="346">
+            </body>
+            </html>
+            <html> <body> <p>Hi,<br>Check out the new post on the api city blog:</p> <p><a href="https://servicodados.ibge.gov.br/api/v1/localidades/estados/{sigla_city.upper()}">Test API: Cloud-based or City API!!</a></p> <p> Feel free to <strong>let us</strong> know what content would be useful for you!</p> </body> </html> <pre>
+            <p style="font-size:30px">"""+sigla +"""</p>
+            """+ body +"""
+      """
       gmail_user = ''
       gmail_password = ''
       to = ''
       message = MIMEMultipart("alternative")
-      sigla_city = states(city.upper())
-      message["Subject"] = f'State {sigla_city}'
+      message["Subject"] = f'State {sigla}'
       message["From"] = gmail_user
       message["To"] = to
 
-      body = str(get(url))
-      with open("demofile.txt", "r") as read:
-            bodys = read.read()
       sent_from = gmail_user
       try:
-            message.attach(MIMEText(bodys, "html"))
+            message.attach(MIMEText(html, "html"))
             server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             server.ehlo()
             server.login(gmail_user, gmail_password)
@@ -78,4 +70,4 @@ def send_email(url):
             print("Failed to send email")
 
 if __name__ == "__main__":
-      send_email(url_ibge)
+      send_email(arg)
